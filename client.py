d8e8e67287fe53dbf5df9511d78d9f7f55703ee0
ILKER AYCICEK <e2098770@cenginek>
2018-11-29 16:57:36 +0300

    Implement destination node

--- client.py
+++ client.py
@@ -1,16 +1,18 @@
-#echo client program
 import socket
+import json
+import time
 
-HOST = '127.0.0.1'
-PORT = 50007
+HOST, PORT = "localhost", 8888
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((HOST,PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while 1:
-    inp = raw_input("")
-    s.sendall(inp)
-    data = s.recv(1024)
-    print 'received back from server',repr(data)
-s.close()
-
+while True:
+    data = raw_input("Send:")
+    sent_time = float(time.time())
+    sock.sendto(data + "\n", (HOST, PORT))
+    received = sock.recv(1024)
+    dest_received_time = json.loads(received)['timestamp'] # timestamp of the message
+    message = json.loads(received)['message'] # actual message from the server
+    print("Sent at :{}".format(sent_time))
+    print("Received at destination at:{}".format(dest_received_time))
+    #print("End-to-end delay : " + str(dest_received_time - sent_time) + " ms")
