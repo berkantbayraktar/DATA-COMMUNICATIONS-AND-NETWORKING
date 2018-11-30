8e456810ad5fef6cccaaba944f221874adaa1070
ilker <ilkeraycicek36@gmail.com>
2018-11-30 21:22:36 +0300

    xd

--- ssh.sh
+++ ssh.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 host=pc3.instageni.rnet.missouri.edu
 user=e2098770
 
+
+
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_geni_ssh_rsa 
-for port in 25571 25573 25572 25570 25574
+for port in 25571 25573 25572 25570 25574 
 do
     gnome-terminal -x bash -c "ssh ${user}@${host} -p ${port} 'python *.py';exec bash" #destination
 done
-#ssh ${user}@${host} -p 25573 # router2
-#ssh ${user}@${host} -p 25572 # router1
-#ssh ${user}@${host} -p 25570 # broker
-#ssh ${user}@${host} -p 25574 # source
