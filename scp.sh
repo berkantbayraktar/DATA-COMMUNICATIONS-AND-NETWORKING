fc1ec34ca3079c4df52a8b7f2943c6f616d74cd4
ilker <ilkeraycicek36@gmail.com>
2018-11-30 20:20:17 +0300

    Modification on scp script

--- scp.sh
+++ scp.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-directory=/home/ilker/Desktop/courses/435\ -\ network/tp-part1/ceng435--term-project/
+directory=/home/ilker/Desktop/courses/435-network/tp-part1/ceng435--term-project/
 host=pc3.instageni.rnet.missouri.edu
 user=e2098770
-
 
 
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_geni_ssh_rsa 
 
-scp -P 25571 {$directory}'destination.py' {$user}@{$host}':/users/'{$user}
-scp -P 25572 {$directory}'r1.py' {$user}@{$host}':/users/'{$user}
-scp -P 25573 {$directory}'r2.py' {$user}@{$host}':/users/'{$user}
-scp -P 25570 {$directory}'broker.py' {$user}@{$host}':/users/'{$user}
-scp -P 25574 {$directory}'source.py' {$user}@{$host}':/users/'{$user}
+scp -P 25571 ${directory}'destination.py' ${user}@${host}':/users/'${user}
+scp -P 25572 ${directory}'r1.py' ${user}@${host}':/users/'${user}
+scp -P 25573 ${directory}'r2.py' ${user}@${host}':/users/'${user}
+scp -P 25570 ${directory}'broker.py' ${user}@${host}':/users/'${user}
+scp -P 25574 ${directory}'source.py' ${user}@${host}':/users/'${user}
--- ssh.sh
+++ ssh.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#hostname = "pc3.instageni.rnet.missouri.edu"
-#user = "e2098770"
+
+host = pc3.instageni.rnet.missouri.edu
+user = e2098770
 
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_geni_ssh_rsa 
 
-ssh e2098770@pc3.instageni.rnet.missouri.edu -p 25571 # destination
-ssh e2098770@pc3.instageni.rnet.missouri.edu -p 25573 # router2
-ssh e2098770@pc3.instageni.rnet.missouri.edu -p 25572 # router1
-ssh e2098770@pc3.instageni.rnet.missouri.edu -p 25570 # broker
-ssh e2098770@pc3.instageni.rnet.missouri.edu -p 25574 # source
+ssh ${user}@${host} -p 25571 # destination
+ssh ${user}@${host} -p 25573 # router2
+ssh ${user}@${host} -p 25572 # router1
+ssh ${user}@${host} -p 25570 # broker
+ssh ${user}@${host} -p 25574 # source
