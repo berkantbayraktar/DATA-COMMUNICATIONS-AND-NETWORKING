bcd49a289798b850d340116f3f00eef3bf092bef
ilker <ilkeraycicek36@gmail.com>
2018-11-30 22:56:44 +0300

    Generate small files

---
+++ file_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+big_file = open("demofile.txt","r")
+little_file = open("demofile_light.txt","w+")
+counter = 10000
+for i in range(0,10000):
+    line = big_file.readline()
+    little_file.write(line)
+    counter-=1
+    if(counter == 0):
+        break
